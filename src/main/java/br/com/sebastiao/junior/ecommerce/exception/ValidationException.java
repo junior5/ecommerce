0e07f834ea1bfb9156cfa2cd599c4b16f9eece10
Junior <saoj07@gmail.com>
2019-02-06 18:48:15 -0200

    Renomeado nome da classe de exceção.

---
+++ src/main/java/br/com/sebastiao/junior/ecommerce/exception/ValidationException.java
@@ -0,0 +1,22 @@
+package br.com.sebastiao.junior.ecommerce.exception;
+
+public class ValidationException extends RuntimeException {
+
+    private static final long serialVersionUID = 5861310537366287163L;
+
+    public ValidationException() {
+        super();
+    }
+
+    public ValidationException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public ValidationException(final String message) {
+        super(message);
+    }
+
+    public ValidationException(final Throwable cause) {
+        super(cause);
+    }
+}
